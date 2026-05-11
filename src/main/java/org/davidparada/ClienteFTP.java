533bdcfa0ab66fe18d4d7a09475968abe778b98a
DavidParadaAmoedo1976 <davidparadadam@gmail.com>
2026-05-11 22:20:32 +0200

    terminado

--- src/main/java/org/davidparada/ClienteFTP.java
+++ src/main/java/org/davidparada/ClienteFTP.java
@@ -1,8 +1,6 @@
 package org.davidparada;
 
-
 import org.apache.commons.net.ftp.*;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -17,9 +15,12 @@ public class ClienteFTP {
         String clave = "anonymous";
 
         try {
+            // Nos conectamos al servidor con los datos añadidos en la variable
             cliente.connect(servidorFtp);
+            // Nos conectamos en modo pasivo
             cliente.enterLocalPassiveMode();
 
+            // comprueba que el usuario y contraseña introducidos sean validos
             boolean login = cliente.login(usuario, clave);
             if (login) {
                 System.out.println("Login correcto ...");
@@ -28,55 +29,73 @@ public class ClienteFTP {
                 cliente.disconnect();
                 System.exit(1);
             }
+            // Indica en consola en que directorio estamos
             System.out.println("Directorio actual: " + cliente.printWorkingDirectory());
 
+            // Indica cuantos elementos hay dentro del directorio actual y los guarda en un array.
             FTPFile[] files = cliente.listFiles();
             System.out.println("Ficheros en el directorio actual: " + files.length);
 
+            // Creamos un array para que muestre los tipos con palabras en vez de un número
             String[] tipos = {"Fichero", "Directorio", "Enlaze simb"};
+            // recorre todo el directorio, y nos dice si son archivos, directorios, o enlaces, y los muestra en la consola
             for (int i = 0; i < files.length; i++) {
                 System.out.printf("%15s => %s\n", files[i].getName(), tipos[files[i].getType()]);
             }
             System.out.println();
+
+            // Vamos a recorrer todo el array para comprobar los directorios, creamos variable para guardar el nombre del directorio.
             String nombreDirectorio = "";
             for (FTPFile f : files){
+                // Si el elemento es un directorio, añade el nombre a la variable y comrueva si esta vacio.
                 if (f.isDirectory()) {
                     nombreDirectorio = f.getName();
                     if (!nombreDirectorio.isEmpty()) {
+                        // Si no esta vacio, entra dentro de el y muestra en consola en que directorio ha entrado.
                         cliente.changeWorkingDirectory(nombreDirectorio);
 
                         System.out.println("Cambiado al directorio: " + cliente.printWorkingDirectory());
 
+                        // Crea un array con los elementos dentro del nuevo directorio.
                         FTPFile[] filesDirectorio = cliente.listFiles();
-                        System.out.println("Contenido de " + nombreDirectorio + ":");
+                        System.out.println("Archivos de " + nombreDirectorio + ":");
 
+                        // Ahora recorremos el directorio y comprurba que sean archivos, si encuentra archivos los muestra si lo
+                        // que encuentra no son archivos, muestra mensaje y cambia de directorio.
                         boolean encontrado = false;
                         String ficheroParaBajar = "";
                         for (int i = 0; i < filesDirectorio.length; i++) {
                             if (filesDirectorio[i].isFile()) {
                                 System.out.printf("Archivo encontrado: %s\n", filesDirectorio[i].getName());
+                                // Guarda el nombre del archivo en una variable.
                                 ficheroParaBajar = filesDirectorio[i].getName();
                                 encontrado = true;
                             }
                         }
 
+                        // Si ha encontrado archivos, en la variable esta el nombre del ultimo archivo
                         if (encontrado) {
                             System.out.println("\nListado de ficheros completado.");
                             cliente.setFileType(FTP.BINARY_FILE_TYPE);
                             System.out.println("Iniciando descarga de: " + ficheroParaBajar);
 
+                            // Ponemos nombre a una variable para crear un directorio en nustro directorio del programa
                             String nombreCarpeta = "descargas";
                             File directorio = new File(nombreCarpeta);
 
+                            // Si el directorio no existe lo crea.
                             if (!directorio.exists()) {
                                 if (directorio.mkdirs()) {
                                     System.out.println("Directorio creado: " + nombreCarpeta);
                                 }
                             }
 
+                            // Crea un archivo con el nombre del archivo que vamos a descargar dento del directorio que hemos creado.
                             File archivoLocal = new File(directorio, ficheroParaBajar);
+                            // Descarga el archivo seleccionado y guarda los datos en el archivo descargado en el archivo que hemos creado
                             try (FileOutputStream ficheroDescargado = new FileOutputStream(archivoLocal)){
                                 boolean descargado = cliente.retrieveFile(ficheroParaBajar,ficheroDescargado);
+                                // Muestra mensaje de si ha tenido exito o no.
                                 if (descargado) {
                                     System.out.println("\nArchivo descargado con exito");
                                 } else {
@@ -86,6 +105,7 @@ public class ClienteFTP {
                                 System.out.println("Error de descarga: " + e.getMessage());
                             }
                             break;
+                        // Muestra en consola si no ha enco0ntrado ningún archivo dentro del directorio
                         } else {
                             System.out.println("No se encontraron archivos en " + nombreDirectorio);
                             cliente.changeToParentDirectory();
@@ -94,12 +114,14 @@ public class ClienteFTP {
                 }
             }
 
+            // Salimos del servidor
             boolean logout = cliente.logout();
             if (logout) {
                 System.out.println("\nLogout del servidor FTP ...");
             } else {
                 System.out.println("Error al hacer logout ...");
             }
+            // Nos desconectamos
             cliente.disconnect();
             System.out.println("\n*** Desconectado ***");
 
